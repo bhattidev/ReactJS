8d13fe1ce98e37bf1bc2f915856abc262aed14c2
Muhammad Imran <imran5244513@gmail.com>
2024-04-30 12:25:30 +0500

    Toptal

--- Toptal/src/layouts/Header.jsx
+++ Toptal/src/layouts/Header.jsx
@@ -1,56 +1,66 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/img/logo.png';
 
 export function Header() {
   return (
     <>
-      <div className=' bg-bgGray relative z-10 overflow-hidden'>
-        <div className=' px- flex justify-between align-baseline h-[64px] px-10 py-3'>
-          <div className='flex justify-between'>
-            <img src={logo} className='px-3 pb-1' alt='logo' width='100px' />
+      <div className=' bg-bgGray relative z-10 h-[64px] w-full'>
+        <div className=' px-[5%]'>
+          <div className='flex'>
+            <img src={logo} className='my-4' alt='logo' width='100px' />
 
-            <div id='nav-links' className=' px-3 mt-2'>
-              <a href='' className='px-3 py-7'>
-                Top 3%
-              </a>
-              <a href='' className='px-3 py-7'>
-                Hire Talent
-                <FontAwesomeIcon icon={faCoffee} />
-                <div id='sub-nav-links'>
-                  <a href=''>Developers</a>
-                  <a href=''>Designers</a>
-                  <a href=''>Marketing Experts</a>
-                  <a href=''>Product Managers</a>
-                  <a href=''>Project Managers</a>
-                  <a href=''>Finance Experts</a>
-                </div>
-              </a>
-              <a href='' className='px-3 py-7'>
-                Why
-              </a>
-              <a href='' className='px-3 py-7'>
-                Clients
-              </a>
-              <a href='' className='px-3 py-7'>
-                Blog
-              </a>
-              <a href='' className='px-3 py-7'>
-                About
-              </a>
-            </div>
+            <nav>
+              <div id='nav-links' className=' px-3'>
+                <a href='' className='px-3 py-4'>
+                  Top 3%
+                </a>
+
+                <a href='' className='px-3 py-5 relative' id='dropdown'>
+                  Hire Talent
+                  <FontAwesomeIcon
+                    icon={faArrowDown}
+                    className=' size-2 px-1'
+                  />
+                  <div
+                    id='dropdown-list'
+                    className='flex bg-white left-0 mt-5 absolute'
+                  >
+                    <a href=''>Developers</a>
+                    <a href=''>Designers</a>
+                    <a href=''>Marketing Experts</a>
+                    <a href=''>Product Managers</a>
+                    <a href=''>Project Managers</a>
+                    <a href=''>Finance Experts</a>
+                  </div>
+                </a>
+
+                <a href='' className='px-3 py-5'>
+                  Why
+                </a>
+                <a href='' className='px-3 py-5'>
+                  Clients
+                </a>
+                <a href='' className='px-3 py-5'>
+                  Blog
+                </a>
+                <a href='' className='px-3 py-5'>
+                  About
+                </a>
+              </div>
+            </nav>
           </div>
 
-          <div className='mt-2'>
-            <a href='' className='p-3 py-7'>
+          <div className='float-end flex align-baseline'>
+            <a href='' className=''>
               Apply as a Freelance
             </a>
             <span>
-              <a href='' className='p-3 py-7'>
+              <a href='' className=''>
                 Hire Top Talent
               </a>
             </span>
-            <a href='' className='p-3 py-7'>
+            <a href='' className=''>
               Log In
             </a>
           </div>
