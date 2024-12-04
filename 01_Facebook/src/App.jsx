bac8aab052e001117d795f06427657809a04b1fa
Bhattii <imran5244513@gmail.com>
2024-12-04 22:17:06 +0500

    Social app

--- 01_Facebook/src/App.jsx
+++ 01_Facebook/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 function App() {
 	return (
 		<>
-			<h1 className="text-red-400">Heading</h1>
+			<Header />
+			<Sidebar />
+			<Footer />
 		</>
 	);
 }
