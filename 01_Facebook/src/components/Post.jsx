d29eb235f69c1129653009a1c6cd8cb4aade1c0c
Bhattii <imran5244513@gmail.com>
2024-12-07 22:24:52 +0500

    react icons install

--- 01_Facebook/src/components/Post.jsx
+++ 01_Facebook/src/components/Post.jsx
@@ -7,6 +7,10 @@ function Post({ post }) {
 				key={post.id}
 				className="card w-[18rem] mx-5 my-12 p-2 border">
 				<div className="card-body">
+					<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+						{post.reaction}
+						<span class="visually-hidden">unread messages</span>
+					</span>
 					<h5 className="card-title">{post.title}</h5>
 					<p className="card-text">{post.body}</p>
 					{post.tags.map((tag) => (
