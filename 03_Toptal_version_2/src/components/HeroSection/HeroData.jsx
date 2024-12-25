6f759587f808d33daa9253998e6af6e5418e4bd3
Bhattii <imran5244513@gmail.com>
2024-12-25 21:10:08 +0500

    update

--- 03_Toptal_version_2/src/components/HeroSection/HeroData.jsx
+++ 03_Toptal_version_2/src/components/HeroSection/HeroData.jsx
@@ -8,20 +8,32 @@ import image6 from '../../assets/images/DeveloperImages/images6.avif';
 import image7 from '../../assets/images/DeveloperImages/images7.avif';
 
 function HeroData() {
-	const devImages = [image1, image2, image3, image4, image5, image6, image7];
+	const devData = [
+		{
+			id: ,
+			name: ,
+			src: ,
+			description: ,
+		}
+		image1, image2, image3, image4, image5, image6, image7];
 	return (
-		<>
-			<div className="">
-				{/* {devImages.map((image, index) => (
+		<div>
+			{devData.map((image, index) => (
+				<>
+					<div
+						className="bg-white "
+						key={index}>
+						abc
+					</div>
 					<img
-						className="absolute"
+						className="absolute sm:w-96 lg:w-[580px] bottom-0 "
 						src={image}
 						alt={index}
 						key={index}
 					/>
-				))} */}
-			</div>
-		</>
+				</>
+			))}
+		</div>
 	);
 }
 
--- 03_Toptal_version_2/src/components/HeroSection/HeroSection.jsx
+++ 03_Toptal_version_2/src/components/HeroSection/HeroSection.jsx
@@ -5,11 +5,11 @@ import HeroData from './HeroData';
 function HeroSection() {
 	return (
 		<>
-			<div className="flex flex-col sm:flex-row bg-gray-400 py-10 px-10">
-				<div className="w-1/2">
+			<div className="flex flex-col sm:flex-row bg-gray-400 py-10 px-10 relative">
+				<div className="w-1/3 pb-20">
 					<HireTheTopMain />
 				</div>
-				<div className="w-1/2">
+				<div className="w-2/3 ">
 					<HeroData />
 				</div>
 			</div>
